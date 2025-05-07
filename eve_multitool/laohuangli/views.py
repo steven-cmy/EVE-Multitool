3843d247669b94997e19fa697d8c015a741e3b6f
steven-cmy <4248629+steven-cmy@users.noreply.github.com>
2025-05-07 16:21:38 -0700

    lhl

--- eve_multitool/laohuangli/views.py
+++ eve_multitool/laohuangli/views.py
@@ -16,9 +16,22 @@ def laohuangli():
     today = utils.today().get_sixty_cycle_day()
     now = utils.now().get_sixty_cycle_hour()
     context = {
+        "time": now.get_solar_time().get_name(),
+        "date": today.get_solar_day().get_day(),
+        "today": today.get_name(),
+        "now": now.get_name(),
+        "luck": today.get_solar_day()
+        .get_lunar_day()
+        .get_minor_ren()
+        .get_luck()
+        .get_name(),
         "day_recommends": utils.translate_taboo_list(today.get_recommends()),
         "day_avoids": utils.translate_taboo_list(today.get_avoids()),
         "hour_recommends": utils.translate_taboo_list(now.get_recommends()),
         "hour_avoids": utils.translate_taboo_list(now.get_avoids()),
+        "battle_location": "",
+        "lucky_ship": "",
+        "lucky_region": "",
+        "direction": today.get_jupiter_direction().get_name(),
     }
     return render_template("laohuangli/laohuangli.html", context=context)
