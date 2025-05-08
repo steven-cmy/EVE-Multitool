25bc746d8c8e3e408c4f49c60ea9e648549235a7
steven-cmy <4248629+steven-cmy@users.noreply.github.com>
2025-05-08 08:54:47 -0700

    develop: reset

--- app/laohuangli/views.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-
-
-blueprint = Blueprint("laohuangli", __name__, url_prefix="/lhl/")
-
-
-@blueprint.route("/")
-def laohuangli():
-    return render_template("laohuangli/lhl.html")
