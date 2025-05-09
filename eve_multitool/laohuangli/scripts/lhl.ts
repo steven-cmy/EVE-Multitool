d944ace9ef168e2f1027c5e59929f998be4c438f
steven-cmy <4248629+steven-cmy@users.noreply.github.com>
2025-05-08 17:20:22 -0700

    local ts

--- eve_multitool/laohuangli/scripts/lhl.ts
+++ eve_multitool/laohuangli/scripts/lhl.ts
@@ -1,1 +1,49 @@
-console.log("XSWL");
+import { SixtyCycleDay, SixtyCycleHour, SolarTime } from 'tyme4ts';
+interface ElementUpdate {
+    id: string;
+    textContent: string;
+}
+
+function tabooTranslate(): string {
+    return ""
+}
+
+
+function update(id: string, t: string): void {
+    const e = document.getElementById(id);
+    if (e) {
+        e.textContent = t;
+    }
+}
+
+function updateAll(): void {
+    let d = new Date();
+    let local_time: SolarTime = SolarTime.fromYmdHms(d.getFullYear(), d.getMonth(), d.getDay(), d.getHours(), d.getMinutes(), d.getSeconds());
+    let now: SixtyCycleHour = local_time.getSixtyCycleHour()
+    let today: SixtyCycleDay = local_time.getSolarDay().getSixtyCycleDay()
+    console.log(now.toString())
+
+    const elementsToUpdate: ElementUpdate[] = [
+        { id: 'time', textContent: now.getSolarTime().getName() },
+        { id: 'hour', textContent: now.getName() },
+        { id: 'yearmonth', textContent: today.getSolarDay().get },
+        { id: 'day', textContent: today.getSolarDay().getDay().toString() },
+        { id: 'today', textContent: today.getName() },
+        // { id: 'day_recommends', textContent: today.getRecommends(). },
+        // { id: 'hour_recommends', textContent: now.getName() },
+        { id: 'luck', textContent: today.getSolarDay().getLunarDay().getMinorRen().getLuck().getName() },
+        // { id: 'day_avoids', textContent: now.getName() },
+        // { id: 'hour_avoids', textContent: now.getName() },
+        { id: 'battle_location', textContent: "-" },
+        { id: 'lucky_ship', textContent: "-" },
+        { id: 'lucky_region', textContent: "-" },
+    ];
+
+    elementsToUpdate.forEach(element => {
+        update(element.id, element.textContent)
+    })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    updateAll();
+});
