96ab9179a7f2f0d41b66b9b44831e1946cbcc0dd
steven-cmy <4248629+steven-cmy@users.noreply.github.com>
2025-06-16 13:08:19 -0700

    feat: 添加EIS图像服务，重构TypesView以使用新服务获取图像URL

--- src/views/esiTypes/TypesView.vue
+++ src/views/esiTypes/TypesView.vue
@@ -1,16 +1,22 @@
 <script setup lang="ts">
 import { onMounted, reactive } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
+import { type IMAGE, getImageUrl } from '@/api/eis';
 import { processEVEMarkup } from '@/utils/esiUtil';
 import getUniverseTypesTypeId from '@/services/getUniverseTypesTypeId';
 
 const type = reactive<{ type_id?: number; name?: string; description?: string }>({});
 const route = useRoute();
 const router = useRouter();
+const typeId = parseInt(route.params.typeid as string);
+const icon: IMAGE = {
+  category: 'types',
+  id: typeId,
+};
 
 onMounted(async () => {
-  if (route.params.typeid) {
-    const res = getUniverseTypesTypeId(parseInt(route.params.typeid as string));
+  if (typeId) {
+    const res = getUniverseTypesTypeId(typeId);
     Object.assign(type, (await res).data);
   }
 });
@@ -18,7 +24,7 @@ onMounted(async () => {
 
 <template>
   <main v-if="$route.params.typeid">
-    <img v-if="type.type_id" :src="`https://images.evetech.net/types/${type.type_id}/icon`" />
+    <img v-if="type.type_id" :src="getImageUrl(icon)" />
     <h1 v-if="type.name">{{ type.name }}</h1>
     <p v-if="type.description" v-html="processEVEMarkup(type.description, router)"></p>
   </main>
