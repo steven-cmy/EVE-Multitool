501164381cc4ab8b9b93eaeea939b3eaede32d1b
steven-cmy <4248629+steven-cmy@users.noreply.github.com>
2025-08-20 14:52:40 -0700

    tidy up

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,24 +9,24 @@ interface RouteModule {
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: (
-    [
-      {
-        path: '/',
-        name: 'home',
-        component: () => import('../views/HomeView.vue'),
-      },
-      {
-        path: '/about',
-        name: 'about',
-        component: () => import('../views/AboutView.vue'),
-      },
-    ] as RouteRecordRaw[]
-  ).concat(
-    await Promise.all(routeModules).then((results) =>
-      results.flatMap((result) => (result as RouteModule).default || []),
-    ),
-  ),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/HomeView.vue'),
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/AboutView.vue'),
+    },
+  ] as RouteRecordRaw[],
+});
+
+// Load dynamic routes after router creation
+Promise.all(routeModules).then((results) => {
+  const dynamicRoutes = results.flatMap((result) => (result as RouteModule).default || []);
+  dynamicRoutes.forEach(route => router.addRoute(route));
 });
 
 export default router;
