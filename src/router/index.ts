538d6fce4b510184321834efce27cbe60cb038bf
cmy <4248629+steven-cmy@users.noreply.github.com>
2025-08-20 15:11:23 -0700

    tidy up and revert error (#20)
    * fix build target
    
    * disabled e2e test for now
    
    * tidy up
    
    * fix dynamic import routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,32 +1,25 @@
-import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router';
-
-const modules = import.meta.glob(['@/router/**/*.ts', '!@/router/index.ts']);
-const routeModules = Object.values(modules).map((module) => module());
-
-interface RouteModule {
-  default?: RouteRecordRaw[];
-}
+import { createRouter, createWebHistory } from 'vue-router';
+import sso from '@/router/sso';
+import types from '@/router/types';
+import epoch from '@/router/epoch';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: (
-    [
-      {
-        path: '/',
-        name: 'home',
-        component: () => import('../views/HomeView.vue'),
-      },
-      {
-        path: '/about',
-        name: 'about',
-        component: () => import('../views/AboutView.vue'),
-      },
-    ] as RouteRecordRaw[]
-  ).concat(
-    await Promise.all(routeModules).then((results) =>
-      results.flatMap((result) => (result as RouteModule).default || []),
-    ),
-  ),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/HomeView.vue'),
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/AboutView.vue'),
+    },
+    ...sso,
+    ...types,
+    ...epoch,
+  ],
 });
 
 export default router;
